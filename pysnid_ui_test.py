ede11a5a3ff54de9b10c020d4660e87858ed24a6
P-Rey <35781757+P-Rey@users.noreply.github.com>
2018-09-17 16:14:04 +0100

    Add files via upload

--- pysnid_ui_test.py
+++ pysnid_ui_test.py
@@ -163,7 +163,7 @@ input_spectrum = parsereadspec()
 template_spectrum = readspec('SN2011fe_2011-08-25_00-00-00_TNG_DOLORES_PTF.ascii')
 #input_spectrum=readspec('2004et_20041027_3299_9327_00.dat')
 
-#Plot the spectra
+#Plot the spectraBOTTOM
 plt.plot(input_spectrum[:,0], input_spectrum[:,1])
 plt.plot(template_spectrum[:,0], template_spectrum[:,1])
 
@@ -211,6 +211,11 @@ correlation = signal.correlate(log_input_spectrum, log_template_spectrum, mode='
 
 plt.plot(correlation, label="")
 plt.legend()
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2TkAgg)
+## Implement the default Matplotlib key bindings.
+#from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
 #plt.show()
 plt.figure()
 #plt.savefig("04et_11fe.pdf")
@@ -225,48 +230,191 @@ print ((leng/2)-maxi)
 print (leng/2)
 
 ###################################################################
+import tkinter as tk 
+import matplotlib.backends.backend_tkagg as tkagg
+from tkinter import ttk
+from tkinter import *
+LARGE_FONT= ("Verdana", 12)
+NORM_FONT= ("Verdana", 10)
+SMALL_FONT= ("Verdana", 8)
 
+
+f = Figure()
+a = f.add_subplot(111)
+
+def popupmsg(msg):
+    popup = tk.Tk()
+    popup.wm_title("!")
+    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+    
+def rlappopup():
+    root = tk.Tk()
+#    popup = tk.Tk()
+    root.wm_title("rlaps")
+    S = tk.Scrollbar(root)
+    T = tk.Text(root, height=4, width=50)
+    S.pack()
+    T.pack()
+    S.config(command=T.yview)
+    T.config(yscrollcommand=S.set)
+    quote = """HAMLET: To be, or not to be--that is the question:
+    Whether 'tis nobler in the mind to suffer
+    The slings and arrows of outrageous fortune
+    Or to take arms against a sea of troubles
+    And by opposing end them. To die, to sleep--
+    No more--and by a sleep to say we end
+    The heartache, and the thousand natural shocks
+    That flesh is heir to. 'Tis a consummation
+    Devoutly to be wished."""
+#    T.insert()
+    T.insert(tk.END, quote)
+    B1 = ttk.Button(root, text="Okay", command = root.destroy)
+    B1.pack()
+    root.mainloop()
+    
+class Application(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+#        window = tk.Tk()
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        #tk.Tk.iconbitmap(self, default="clienticon.ico")
+        tk.Tk.wm_title(self, "PySNID")
+        
+        
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+
+        menubar = tk.Menu(container)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label = "rlap values", command = lambda: rlappopup())
+        filemenu.add_separator()
+        filemenu.add_command(label="Save settings", command = lambda: popupmsg("Not supported just yet!"))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        tk.Tk.config(self, menu=menubar)
+
+
+        fig, ax1  = plt.subplots(1,1)
+        ax1.plot(input_spectrum[:,0],input_spectrum[:,1], label = 'User Spectrum')
+        ax1.plot(template_spectrum[:,0],template_spectrum[:,1], label = 'Template Spectrum')
+        ax1.set_title('Input v Best Fit Template')
+        ax1.set_ylabel('Normalised Flux (arb. units)')
+        ax1.set_xlabel('Wavelength (Angstrom)')
+        ax1.legend()
+
+        
+        canvas = FigureCanvasTkAgg(fig, master=container)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=0)
+        tkagg.NavigationToolbar2Tk(canvas, container)
+        
+        OPTIONS = [
+        'sn1980K',
+        'sn1989B',
+        'sn2004et',
+        'sn2012aw',
+        ] #etc
+        
+        #master = Tk()
+        
+        variable = tk.StringVar(container)
+        variable.set(OPTIONS[0]) # default value
+        
+        w = tk.OptionMenu(container, variable, *OPTIONS)
+        w.pack()
+        def glines(ax1):
+            ax1.vlines(x= 6500,ymin =-1,ymax = 2, colors = 'm',linestyles = 'dashdot', label = 'Hydrogen Alpha' )
+        b = tk.Button(container, text = 'Galaxy lines', command = lambda:glines(ax1))
+        b.pack()
+#        self.frames = {}
+
+#        for F in (GraphPage):
+#
+#            frame = F(container, self)
+#
+#            self.frames[F] = frame
+#
+#            frame.grid(row=0, column=0, sticky="nsew")
+#
+#        self.show_frame(GraphPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class GraphPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(GraphPage))
+        button1.pack()
+
+        
+
+        
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+app = Application()
+
+#app.geometry('1280x720')
+
+app.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################
+'''
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2TkAgg)
 ## Implement the default Matplotlib key bindings.
 #from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
-#from six.moves import tkinter as Tk
-#
-#root = Tk.Tk()
-#root.wm_title("Embedding in Tk")
-#
-#fig = Figure(figsize=(5, 2), dpi=100)
-#t = np.arange(0, 3, .01)
-#fig.add_subplot(111).plot( correlation)
-#fig.add_subplot(111).plot( log_template_spectrum)
-#canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-#canvas.draw()
-#canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-#
-#toolbar = NavigationToolbar2TkAgg(canvas, root)
-#toolbar.update()
-#canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-#
-#
-#def on_key_press(event):
-#    print("you pressed {}".format(event.key))
-#    key_press_handler(event, canvas, toolbar)
-#
-#
-#canvas.mpl_connect("key_press_event", on_key_press)
-#
-#
-#def _quit():
-#    root.quit()     # stops mainloop
-#    root.destroy()  # this is necessary on Windows to prevent
-#                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
-#
-#
-#button = Tk.Button(master=root, text="Quit", command=_quit)
-#button.pack(side=Tk.BOTTOM)
-#
-#Tk.mainloop()
+
 
 ###########################
 
@@ -274,25 +422,61 @@ import tkinter as tk
 from matplotlib import style
 import matplotlib.backends.backend_tkagg as tkagg
 LARGE_FONT= ("Verdana", 12)
-#style.use("ggplot")
-#
-#fig = Figure(figsize=(10, 4), dpi=100)
-#a = fig.add_subplot(111)
-#
-#
-#def animate(i):
-#    pullData = open('sampleText.txt','r').read()
-#    dataArray = pullData.split('\n')
-#    xar=[]
-#    yar=[]
-#    for eachLine in dataArray:
-#        if len(eachLine)>1:
-#            x,y = eachLine.split(',')
-#            xar.append(int(x))
-#            yar.append(int(y))
-#    a.clear()
-#    a.plot(xar,yar)
+
+class PySNID():
+    window = tk.Tk()
+    window.title("PySNID")
+    window.geometry("1270x720")
     
+    menubar = tk.Menu()
+    filemenu = tk.Menu(menubar)
+    filemenu.add_command(label = 'Exit', command = quit)
+    
+    fig, ax1  = plt.subplots(1,1)
+    fig2, ax2 =  plt.subplots(1,1)
+    ax1.plot(input_spectrum[:,0],input_spectrum[:,1])
+    ax1.plot(template_spectrum[:,0],template_spectrum[:,1])
+    ax1.set_title('Input v Best Fit Template')
+    ax1.set_ylabel('Normalised Flux (arb. units)')
+    ax1.set_xlabel('Wavelength (Angstrom)')
+    ax2.plot(correlation)
+    
+    canvas = FigureCanvasTkAgg(fig, master=window)  # A tk.DrawingArea.
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=0)
+    tkagg.NavigationToolbar2Tk(canvas, window)
+    
+    #canvas1 = FigureCanvasTkAgg(fig2, master=window)  # A tk.DrawingArea.
+    #canvas1.draw()
+    #canvas1.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    
+    # canvas is your canvas, and root is your parent (Frame, TopLevel, Tk instance etc.)
+    #tkagg.NavigationToolbar2Tk(canvas1, window)
+    
+    def create_window(window):
+        page = tk.Toplevel(window)
+    
+    def onpress(ax1):
+        #if chk_state.set != True:
+        #        return
+        #else:
+        ax1.vlines(x= 6500,ymin =-1,ymax = 2, colors = 'm',linestyles = 'dashdot', label = 'Hydrogen Alpha' )
+        #ax1.label()
+    chk_state = tk.BooleanVar()
+    chk_state.set(True)
+    #chk = tk.Checkbutton(window, text ='Galaxy Lines', var = onpress(chk_state, ax1))
+    chk = tk.Radiobutton(window, text='Galaxy Lines', variable=onpress(ax1), value=1)
+    #chk = tk.Button(window, text ='Galaxy Lines', command = onpress(ax1))
+    chk.pack()
+    b = tk.Button(window, text= "rlap values", command = create_window(window))
+    b.pack()
+#    chk.grid(column = 0, row  = 0)
+    window.mainloop()
+    
+app = PySNID()
+#app.mainloop()
+'''
+'''
 class SeaofBTCapp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -300,38 +484,37 @@ class SeaofBTCapp(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
 #        tk.Tk.iconbitmap(self, default ="@/Downloads/ucd_brandmark_colour.ico")
         tk.Tk.wm_title(self, "PySNID")
-        container = tk.Frame(self)
 
+        container = tk.Frame(self)BOTTOM
         container.pack(side="top", fill="both", expand = True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        menubar = tk.Menu(container)
+        filemenu = tk.Menu(menubar, tearoff = 0)
+        filemenu.add_command(label = 'Exit', command = quit)
+#        filemenu.add_command(label = 'G-Lines', command = onpress())
+
         self.frames = {}
 
-
-
         frame = StartPage(container, self)
-
-        self.frames[StartPage] = StartPage(container, self) # frame
-
+        self.frames[StartPage] = frame
         frame.grid(row=0, column=0, sticky="nsew")
+      #  self.show_frame(StartPage)
 
-        self.show_frame(StartPage)
+   # def show_frame(self, cont):
 
-    def show_frame(self, cont):
-
-        frame = self.frames[cont]
-        frame.tkraise()
+    #    frame = self.frames[cont]
+     #   frame.tkraise()
 
 
-def qf(quickPrint):
-    print(quickPrint)
+#def qf(quickPrint):
+#    print(quickPrint)
 
         
-class StartPage(tk.Frame):
+#class StartPage(tk.Frame):
 
-    def __init__(self, parent, controller):
+#    def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
 #        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
 #        label.pack(pady=10,padx=10)
@@ -341,36 +524,33 @@ class StartPage(tk.Frame):
 #        button.pack()
         root = tk.Tk()
     
-       # root.wm_title("PySNID")
-        fig = Figure(figsize=(10, 4), dpi=100)
-        fig1 = Figure(figsize=(10, 4), dpi=100)
-
-        fig.add_subplot(111).plot(input_spectrum[:,0],input_spectrum[:,1])
+        root.wm_title("PySNID")
         
-        fig.add_subplot(111).plot(template_spectrum[:,0],template_spectrum[:,1])
-        fig1.add_subplot(121).plot(correlation)
+        fig, ax1  = plt.subplots(1,1)
+        fig2, ax2 =  plt.subplots(1,1)
+        ax1.plot(input_spectrum[:,0],input_spectrum[:,1])
+        ax1.plot(template_spectrum[:,0],template_spectrum[:,1])
+        ax1.set_title('Input v Best Fit Template')
+        ax1.set_ylabel('Normalised Flux (arb. units)')
+        ax1.set_xlabel('Wavelength (Angstrom)')
+        ax2.plot(correlation)
         
-#        figsrc, axsrc = plt.subplots()
-#        figzoom, axzoom = plt.subplots()
-#        axsrc.set(xlim=(0, 1), ylim=(0, 1), autoscale_on=False,title='Click to zoom')
-#        axzoom.set(xlim=(0.45, 0.55), ylim=(0.4, 0.6), autoscale_on=False,title='Zoom window')
-#
-#        def onpress(event):
-#            if event.button != 1:
+        def onpress():
+ #           if event.button != 1:
 #                return
-#            x, y = event.xdata, event.ydata
-#            axzoom.set_xlim(x - 0.1, x + 0.1)
-#            axzoom.set_ylim(y - 0.1, y + 0.1)
-#            figzoom.canvas.draw()
-#        
-#        figsrc.canvas.mpl_connect('button_press_event', onpress)
+            ax1.vlines(x= 6500,ymin =-1,ymax = 2, colors = 'm',linestyles = 'dashdot', label = 'Hydrogen Alpha' )
+            ax1.label()
+     
+        b =tk. Button(root, text = 'Galaxy Lines', command = onpress)
+        b.pack()
+       # fig.canvas.mpl_connect('button_press_event', onpress)
         
         canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
         tkagg.NavigationToolbar2Tk(canvas, root)
         
-        canvas1 = FigureCanvasTkAgg(fig1, master=root)  # A tk.DrawingArea.
+        canvas1 = FigureCanvasTkAgg(fig2, master=root)  # A tk.DrawingArea.
         canvas1.draw()
         canvas1.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
         
@@ -378,7 +558,8 @@ class StartPage(tk.Frame):
         tkagg.NavigationToolbar2Tk(canvas1, root)
 
 app = SeaofBTCapp()
-
+app.geometry("1280x720")
 #ani = animation.FuncAnimation(fig, animate, interval=1000)
 
 app.mainloop()
+'''
